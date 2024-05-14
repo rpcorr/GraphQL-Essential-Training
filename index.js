64a0654043a82aab8b5aeabd6226af1581c82891
Ronan Corr <ronan_corr@hotmail.com>
2024-05-14 14:17:20 -0400

    initial (server) setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const PORT = 8080;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!')
+})
+
+app.listen(PORT, () => console.log(`Running on localhost: ${PORT}/graphql`));
