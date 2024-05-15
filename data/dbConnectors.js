110012c7d209facb7f4d88ac051d62c58ad2ce00
Ronan Corr <ronan_corr@hotmail.com>
2024-05-14 20:53:26 -0400

    put the db connection into its own file

---
+++ data/dbConnectors.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+
+dotenv.config({ path: './config.env' });
+
+// CONNECT TO DATABASE
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD,
+  );
+
+// Connection URI
+const uri = DB; // Change this URI to match your MongoDB server
+
+// Database Name
+const dbName = 'graphQL'; // Change this to your database name
+
+// Create a new MongoClient
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to the MongoDB server
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        console.log('Connected to MongoDB successfully');
+        // Perform operations using db object
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+connectToMongoDB();
+
+const widgetSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    soldout: String,
+    inventory: String,
+    stores: Array,
+});
+
+const Widgets = mongoose.model('widgets', widgetSchema);
+
+export { Widgets };
--- index.js
+++ index.js
@@ -2,41 +2,8 @@ import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './data/schema';
 import resolvers from './data/resolvers';
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
 
 const PORT = 8080;
-
-dotenv.config({ path: './config.env' });
-
-// CONNECT TO DATABASE
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD,
-  );
-
-// Connection URI
-const uri = DB; // Change this URI to match your MongoDB server
-
-// Database Name
-const dbName = 'graphQL'; // Change this to your database name
-
-// Create a new MongoClient
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Connect to the MongoDB server
-async function connectToMongoDB() {
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        console.log('Connected to MongoDB successfully');
-        // Perform operations using db object
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-}
-
-connectToMongoDB();
 
 const app = express();
 
